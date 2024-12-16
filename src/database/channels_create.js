6f10b6255ae63760c4bca7c7f507d0aae94c4209
StarlitEvelyn <evelyn.rei@protonmail.com>
2024-12-16 09:41:26 +0000

    Added permission check for Admin role, [TEMP]

--- src/database/channels_create.js
+++ src/database/channels_create.js
@@ -4,9 +4,14 @@ module.exports = async (title, accesstoken) => {
     let conn;
     try {
         conn = await pool.getConnection();
+        const userId = (await conn.query("SELECT id FROM tokens WHERE token = ?", [accesstoken]))[0].id
+        const roles = (await conn.query("SELECT * FROM roles WHERE id IN (SELECT role_id FROM user_roles WHERE user_id = ?)", [userId]));
 
-        const permissionCheck = true; // TODO implement a permissions check
-        if (!permissionCheck) return { status: 403, message: "Missing permissions!" }
+        let permission = false;
+        roles.forEach(role => {
+            if (role.name == "Admin") permission = true;
+        });
+        if (!permission) return { status: 403, message: "Missing permissions!" }
 
         const channel = (await conn.query("INSERT INTO channels(title) VALUES (?) RETURNING *", [title]))[0];
 
